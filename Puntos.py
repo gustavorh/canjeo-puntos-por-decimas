7a8144c4bb8ecbdb94b1d2dfd0242183f6ee3b06
Gustavo <gustavocars@gmail.com>
2022-09-19 18:31:47 -0300

    Subida de archivos.
    Subida de archivos inicial.

---
+++ Puntos.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 11:03:44 2021
+
+Nombre: Gustavo Reyes Herrera
+RUT: oculto
+
+Mis respuestas en este trabajo son propias, y están realizadas
+en conformidad con la formación ética de lSa universidad.
+
+"""
+
+puntos = input("Ingresa la cantidad de puntos: ")
+puntos = float(puntos)
+
+# 0.6 decimas cada punto
+decimas = 0.6 * puntos
+print(f"Te corresponden '{decimas}' décimas.")
